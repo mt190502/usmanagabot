3c819dff54fb3770d07d2ae29a87892094d958c4
Edip Hzr <medip642@gmail.com>
2024-08-13 23:52:10 +0300

    feat: logger: listen message delete events
    Co-authored-by: Taha <mt190502@mtaha.dev>

--- src/events/message/message_delete.ts
+++ src/events/message/message_delete.ts
@@ -1,27 +1,32 @@
-import { Events, Message } from 'discord.js';
+import { Colors, EmbedBuilder, Events, Message, WebhookClient } from 'discord.js';
 import { DatabaseConnection } from '../../main';
 import { Messages } from '../../types/database/messages';
 import { Event_t } from '../../types/interface/events';
 import { CheckAndAddChannel, CheckAndAddUser } from '../../utils/common';
 import { Logger } from '../../utils/logger';
+import { Guilds } from '../../types/database/guilds';
 
 const exec = async (message: Message) => {
     if (message.author?.bot || !message.author?.id) return;
 
     await CheckAndAddUser(message, null);
     await CheckAndAddChannel(message, null);
-    
-    await DatabaseConnection.manager.findOne(Messages, { where: { message_id: BigInt(message.id) } })
-        .then(async (msgInDB) => {
-            if (!msgInDB) {
-                Logger('warn', 'Message not found in database');
-                return;
-            }
-            msgInDB.message_is_deleted = true;
-            await DatabaseConnection.manager.save(msgInDB);
-        }).catch((err) => {
-            Logger('error', err);
-        });
+
+    const guild = await DatabaseConnection.manager.findOne(Guilds, { where: { gid: message.guild?.id } });
+    const messageInDB = await DatabaseConnection.manager.findOne(Messages, { where: { message_id: BigInt(message.id) } })
+    if (!messageInDB) {
+        Logger('warn', 'Message not found in database');
+        return;
+    }
+    const webhookClient = new WebhookClient({ id: guild.message_logger_webhook_id, token: guild.message_logger_webhook_token });
+ 
+    const embed = new EmbedBuilder().setTitle('Deleted Message').setColor(Colors.Red).setTimestamp();
+    webhookClient.editMessage(messageInDB.logged_message_id.toString(), {
+        embeds: [embed],
+    });
+
+    messageInDB.message_is_deleted = true;
+    await DatabaseConnection.manager.save(messageInDB);
 };
 
 export default {
