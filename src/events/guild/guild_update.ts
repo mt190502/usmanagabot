5ddf974cd0724af380c3ce516ad05e6fab79cc50
Taha <mt190502@mtaha.dev>
2024-10-13 11:24:25 +0300

    fix: disable unused events, add guild save to db method if bot is invited a new guild

--- src/events/guild/guild_update.ts
+++ src/events/guild/guild_update.ts
@@ -1,4 +1,6 @@
 import { Events, Guild } from 'discord.js';
+import { DatabaseConnection } from '../../main';
+import { Guilds } from '../../types/database/guilds';
 import { Event_t } from '../../types/interface/events';
 import { Logger } from '../../utils/logger';
 
@@ -9,5 +11,23 @@ export default {
     data: Events.GuildUpdate,
     execute: async (oldGuild: Guild, newGuild: Guild) => {
         Logger('info', `Guild is updated: "${oldGuild.name}" (${oldGuild.id}) -> "${newGuild.name}" (${newGuild.id})`);
+
+        let success = false;
+        try {
+            const guild = await DatabaseConnection.manager.findOne(Guilds, { where: { gid: BigInt(oldGuild.id) } });
+            if (guild) {
+                guild.name = newGuild.name;
+                await DatabaseConnection.manager.save(guild);
+                success = true;
+            }
+        } catch (error) {
+            Logger('warn', `Failed to update guilds to database: ${error}`);
+        }
+        if (success) {
+            Logger(
+                'info',
+                `Guild "${oldGuild.name} (${oldGuild.id})" updated to "${newGuild.name} (${newGuild.id})" in database`
+            );
+        }
     },
 } as Event_t;
