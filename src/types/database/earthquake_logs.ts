f5bf0cc6fa8e31ae59043164dce4c4b2377e82a9
Taha <mt190502@mtaha.dev>
2025-04-26 22:04:43 +0300

    fix: change earthquake logs table column for multiple servers

--- src/types/database/earthquake_logs.ts
+++ src/types/database/earthquake_logs.ts
@@ -1,9 +1,12 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Guilds } from './guilds';
 
 @Entity()
 export class EarthquakeLogs {
-    @PrimaryColumn({ type: 'varchar', length: 64, nullable: false })
+    @PrimaryGeneratedColumn({ type: 'smallint' })
+    id: number;
+
+    @Column({ type: 'varchar', length: 64, nullable: false })
     source_id: string;
 
     @Column({ type: 'varchar', length: 32, nullable: false })
