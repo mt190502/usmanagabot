14f1db3519056c3370bc38088d42ab91c6685862
Taha <mt190502@mtaha.dev>
2025-11-08 08:29:24 +0300

    feat(decorator): add CommandAction decorator for command execution handling

--- src/types/decorator/command.ts
+++ src/types/decorator/command.ts
@@ -8,3 +8,12 @@ export function CommandSetting(o: { name: string; description: string }): Method
         Reflect.defineMetadata('custom:settings', settings, target_class.constructor);
     };
 }
+
+export function CommandAction(name: string): MethodDecorator {
+    return (target_class, property_key, descriptor_func) => {
+        const exec: Map<string, typeof descriptor_func> =
+            Reflect.getMetadata('custom:command', target_class.constructor) ?? new Map();
+        exec.set(name, descriptor_func);
+        Reflect.defineMetadata('custom:command', exec, target_class.constructor);
+    };
+}
