6826ea296a5f36fb692790bbe76d547ca01f5834
Taha <mt190502@mtaha.dev>
2025-11-15 11:58:04 +0300

    feat(command): implement alias command for managing keyword aliases

--- src/types/database/entities/alias.ts
+++ src/types/database/entities/alias.ts
@@ -4,7 +4,27 @@ import { Guilds } from './guilds';
 import { Users } from './users';
 
 @Entity()
-export class Alias {
+export class AliasSystem {
+    @PrimaryGeneratedColumn({ type: 'smallint' })
+    id!: number;
+
+    @Column({ type: 'boolean', nullable: false, default: false })
+    is_enabled!: boolean;
+
+    @ManyToOne(() => Users, { nullable: false, eager: true })
+    @JoinColumn({ name: 'from_user', referencedColumnName: 'id' })
+    latest_action_from_user!: Users;
+
+    @ManyToOne(() => Guilds, { nullable: false, eager: true })
+    @JoinColumn({ name: 'from_guild', referencedColumnName: 'id' })
+    from_guild!: Guilds;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    timestamp!: Date;
+}
+
+@Entity()
+export class Aliases {
     @PrimaryGeneratedColumn({ type: 'smallint' })
     id!: number;
 
@@ -13,6 +33,15 @@ export class Alias {
 
     @Column({ type: 'varchar', length: 512, nullable: false })
     content!: string;
+
+    @Column({ type: 'boolean', nullable: false, default: false })
+    case_sensitive!: boolean;
+
+    @Column({ type: 'boolean', nullable: false, default: false })
+    consists_only_of_word!: boolean;
+
+    @Column({ type: 'boolean', nullable: false, default: false })
+    use_regex!: boolean;
 
     @ManyToOne(() => Users, { nullable: false, eager: true })
     @JoinColumn({ name: 'from_user', referencedColumnName: 'id' })
